cbc5353e34a128214bb97de7aaae067fa7ea1d34
howardnguyen714 <howardnguyen714@gmail.com>
2021-03-15 15:49:36 -0700

    added profile page

--- main_app/views.py
+++ main_app/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 def home(request):
     return render(request, 'home.html')
+
+def profile(request):
+    return render(request, 'profile.html')
