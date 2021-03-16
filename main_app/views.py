987ab0383ae1f8aca9ac7a35d049d33883480e92
howardnguyen714 <howardnguyen714@gmail.com>
2021-03-16 16:53:02 -0700

    resolved auth bug

--- main_app/views.py
+++ main_app/views.py
@@ -7,14 +7,6 @@ from django.contrib.auth import login
 
 from django.http import HttpResponse
 def home(request):
-  if request.method == 'POST':
-    form = UserCreationForm(request.POST)
-    if form.is_valid():
-      user = form.save()
-      login(request, user)
-      return redirect('index')
-    else:
-      error_message = 'Invalid sign up - try again'
   form = UserCreationForm()
   context = {'form': form}
   return render(request, 'home.html', context)
@@ -29,7 +21,7 @@ def signup(request):
     if form.is_valid():
       user = form.save()
       login(request, user)
-      return redirect('index')
+      return redirect('profile')
     else:
       error_message = 'Invalid sign up - try again'
   form = UserCreationForm()
