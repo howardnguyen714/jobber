ff6491a17f719618b76639859e5775f8f3895fa9
yatziry ortiz <oyatziry@gmail.com>
2021-03-15 17:08:26 -0700

    adds profile model

--- main_app/models.py
+++ main_app/models.py
@@ -1,12 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-<<<<<<< HEAD
-# Create your models here.
-=======
 # Create your models here.
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     join_date = models.DateField('Date User Joined')
->>>>>>> submain
