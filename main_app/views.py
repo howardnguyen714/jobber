0a9091520f112789c5ec05e6f3e55a0d062ac168
howardnguyen714 <howardnguyen714@gmail.com>
2021-03-17 11:41:31 -0700

    added Event import

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login
+from .models import Event
 
 # Create your views here.
 
