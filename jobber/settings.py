5d116f701b44f949b08de54e0b9e6fc1772ce5b5
howardnguyen714 <howardnguyen714@gmail.com>
2021-03-19 10:28:56 -0700

    heroku repairs

--- jobber/settings.py
+++ jobber/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/profile/'
 LOGOUT_REDIRECT_URL = '/'
